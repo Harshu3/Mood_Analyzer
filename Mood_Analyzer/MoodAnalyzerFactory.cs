f9d460ea730faab853d85a23a064c5d54479309c
Harshita <chandaliyaharshita222@gmail.com>
2023-06-09 19:18:52 +0530

    Use reflection to create parameter constructor

--- Mood_Analyzer/MoodAnalyzerFactory.cs
+++ Mood_Analyzer/MoodAnalyzerFactory.cs
@@ -11,30 +11,6 @@ namespace MoodAnalyzerDemo
 {
     public class MoodAnalyzerFactory
     {
-        public object CreateMoodAnalyzerObject(string className, string constructor)
-        {
-            string pattern = "." + constructor + "$";
-            bool result = Regex.IsMatch(className, pattern);
-
-            if (result)
-            {
-                try
-                {
-                    Assembly assembly = Assembly.GetExecutingAssembly();
-                    Type moodAnalyzerType = assembly.GetType(className);
-                    return Activator.CreateInstance(moodAnalyzerType);
-                }
-                catch (Exception ex)
-                {
-                    throw new MoodAnalyzerException("class not found", MoodAnalyzerException.ExceptionTypes.CLASS_NOT_FOUND);
-                }
-            }
-            else
-            {
-                throw new MoodAnalyzerException("constructor not found", MoodAnalyzerException.ExceptionTypes.CONSTRUCTOR_NOT_FOUND);
-            }
-        }
-        
         public object CreateMoodAnalyzerParameterizedObject(string className, string constructor, string message)
         {
             try
